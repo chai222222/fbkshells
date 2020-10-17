72d5c03df2a36354a756928c2d515471c8342ba5
saya <chai.chai1222@gmail.com>
2020-10-17 19:37:18 +0900

    add fbkcsvfilter.sh

---
+++ fbkcsvfilter.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+# csvを必要なデータでフィルタする
+# "-r" オプションにて、フィルタ対象を逆転させる
+#
+
+MODE=1 # プラス1はそのままフィルタする
+if [[ "$1" == "-r" ]]; then
+  MODE=-1 # マイナス1はフィルタを反転する
+fi
+
+awk -F"," -v mode=$MODE -v BINMODE=3 -v ORS='\r\n' -v RS='\r\n' '
+function setArr(str, arr,   narr) {
+  split(str, narr, ",");
+  for (e in narr) arr[narr[e]] = 1;
+}
+BEGIN {
+  fMidCode = "中分類コード";
+  fSmallCode = "小分類コード";
+  fVerySmallCode = "細分類コード";
+  setArr("12", filterStrMid);
+  setArr("1304,1107,1502", filterStrSmall);
+  setArr("111401,110497,111901,150101,150111,150121,150123,150141,150161,150163,150151,150153,150171,150181,150191,150193,150195,150197,199701", filterStrVerySmall);
+}
+NR == 1 {
+  for (i=1; i<=NF; i++) {
+    colIdx[$i] = i;
+  }
+}
+NR > 1 {
+  exist = ($colIdx[fMidCode] in filterStrMid || $colIdx[fSmallCode] in filterStrSmall || $colIdx[fVerySmallCode] in filterStrVerySmall) ? 1 : -1;
+  if (exist * mode > 0) next;
+}
+{
+  print;
+}
+'
+
